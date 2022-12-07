bde9b7f23aceafe1f4a5f69434015741862abf56
wutong21 <18072916289@163.com>
2022-12-07 23:27:39 +0800

    fix: add no null validate

--- index.js
+++ index.js
@@ -27,34 +27,19 @@
  * @property {string} [watchPath]
  */
 
-let Server;
-const proxyFactory = (proxyConfig = {}) => {
-    const proxyRules = Object.entries(proxyConfig);
-    const proxy = proxyRules.map(([match, options]) => {
-        return {
-            changeOrigin: true,
-            onProxyRes: (proxyRes, req) => {
-                proxyRes.headers["x-real-url"] =
-                    options.target.replace(/\/$/, "") + req.url;
-            },
-            ...options,
-            context: match,
-        };
-    });
-    return proxy;
-};
+let Server
 
 try {
-    Server = require("webpack-dev-server");
+    Server = require('webpack-dev-server')
 } catch (e) {
-    console.log("ProxyAutoPlugin Warning: Cannot find module webpack-dev-server, try deliver it via parameter");
+    console.log('ProxyAutoPlugin Warning: Cannot find module webpack-dev-server, try deliver it via parameter')
 }
 
 class ProxyAutoPlugin {
     /** @type {Option} */
-    option;
+    option
     /** @type {number} */
-    baseRouteStackLength;
+    baseRouteStackLength
 
     /**
      * @param {Option} option
@@ -63,110 +48,106 @@ class ProxyAutoPlugin {
     constructor(option, DevServer) {
         if (!option) {
             throw new Error(
-                "ProxyAutoPlugin Error: It seems that you forget to deliver the option parameter, please deliver it and try again"
-
-            );
+                'ProxyAutoPlugin Error: It seems that you forget to deliver the option parameter, please deliver it and try again',
+            )
         }
-        if (typeof option !== "object") {
+        if (typeof option !== 'object') {
             throw new Error(
-                "ProxyAutoPlugin Error: The option parameter should be an object, see https://github.com/wutong21/proxy-auto-plugin"
-
-            );
+                'ProxyAutoPlugin Error: The option parameter should be an object, see https://github.com/wutong21/proxy-auto-plugin',
+            )
         }
-        this.option = option;
+        this.option = option
         if (DevServer) {
-            Server = DevServer;
+            Server = DevServer
         }
         if (!Server) {
             throw new Error(
-                'ProxyAutoPlugin Error: Cannot find Server from parameter, try "npm i webpack-dev-server -D" to solve this problem'
-
-            );
+                'ProxyAutoPlugin Error: Cannot find Server from parameter, try "npm i webpack-dev-server -D" to solve this problem',
+            )
         }
     }
 
     apply() {
-        console.log("ProxyAutoPlugin Started");
+        console.log('ProxyAutoPlugin Started')
 
-        const option = this.option;
+        const option = this.option
 
         // version 3
-        if (typeof Server.prototype.setupFeatures === "function") {
-            const setupFeatures = Server.prototype.setupFeatures;
+        if (typeof Server.prototype.setupFeatures === 'function') {
+            const setupFeatures = Server.prototype.setupFeatures
 
             Server.prototype.setupFeatures = async function () {
-                this.pluginOption = option;
+                this.pluginOption = option
 
                 delete require.cache[require.resolve(this.pluginOption.watchPath)]
                 let content = require(this.pluginOption.watchPath)
-                this.options.proxy = proxyFactory(content.devServer.proxy)
+                if (content.devServer.proxy) {
+                    this.options.proxy = content.devServer.proxy
+                }
 
-                this.baseRouteStackLength = this.app._router.stack.length;
+                this.baseRouteStackLength = this.app._router.stack.length
 
-                setupFeatures.call(this);
+                setupFeatures.call(this)
 
                 if (this.pluginOption.watchPath) {
-                    this._watch(this.pluginOption.watchPath);
-                    const watcher =
-                        this.contentBaseWatchers[this.contentBaseWatchers.length - 1];
-                    watcher.on("change", () => {
+                    this._watch(this.pluginOption.watchPath)
+                    const watcher = this.contentBaseWatchers[this.contentBaseWatchers.length - 1]
+                    watcher.on('change', () => {
                         delete require.cache[require.resolve(this.pluginOption.watchPath)]
                         let content = require(this.pluginOption.watchPath)
-                        this.options.proxy = proxyFactory(content.devServer.proxy);
-                        this.app._router.stack = this.app._router.stack.slice(
-                            0,
-                            this.baseRouteStackLength
-                        );
-                        setupFeatures.call(this);
-                        console.log(
-                            `ProxyAutoPlugin: Successfully switch proxy to '${JSON.stringify(content.devServer.proxy)}'`
-                        );
-                    });
+                        if (content.devServer.proxy) {
+                            this.options.proxy = content.devServer.proxy
+                            this.app._router.stack = this.app._router.stack.slice(0, this.baseRouteStackLength)
+                            setupFeatures.call(this)
+                            console.log(
+                                `ProxyAutoPlugin: Successfully switch proxy to '${JSON.stringify(content.devServer.proxy)}'`,
+                            )
+                        }
+                    })
                 }
-            };
+            }
         } else {
-
             //version 4
-            const setupMiddlewares = Server.prototype.setupMiddlewares;
-            const normalizeOptions = Server.prototype.normalizeOptions;
+            const setupMiddlewares = Server.prototype.setupMiddlewares
+            const normalizeOptions = Server.prototype.normalizeOptions
 
             Server.prototype.normalizeOptions = async function () {
-                this.pluginOption = option;
+                this.pluginOption = option
                 delete require.cache[require.resolve(this.pluginOption.watchPath)]
                 let content = require(this.pluginOption.watchPath)
-                this.options.proxy = proxyFactory(content.devServer.proxy)
-                await normalizeOptions.call(this);
-            };
+                if (content.devServer.proxy) {
+                    this.options.proxy = content.devServer.proxy
+                }
+                await normalizeOptions.call(this)
+            }
 
             Server.prototype.setupMiddlewares = function (middlewares, devServer) {
+                this.baseRouteStackLength = this.app._router.stack.length
 
-                this.baseRouteStackLength = this.app._router.stack.length;
-
-                setupMiddlewares.call(this, middlewares, devServer);
+                setupMiddlewares.call(this, middlewares, devServer)
 
                 if (this.pluginOption.watchPath) {
-                    this.watchFiles(this.pluginOption.watchPath);
-                    const watcher = this.staticWatchers[this.staticWatchers.length - 1];
-                    watcher.on("change", async () => {
+                    this.watchFiles(this.pluginOption.watchPath)
+                    const watcher = this.staticWatchers[this.staticWatchers.length - 1]
+                    watcher.on('change', async () => {
                         delete require.cache[require.resolve(this.pluginOption.watchPath)]
                         let content = require(this.pluginOption.watchPath)
-                        this.options.proxy = proxyFactory(content.devServer.proxy);
-                        await normalizeOptions.call(this);
-                        this.app._router.stack = this.app._router.stack.slice(
-                            0,
-                            this.baseRouteStackLength
-                        );
-                        setupMiddlewares.call(this, middlewares, devServer);
 
-                        console.log(
-                            `ProxyAutoPlugin: Successfully switch proxy to '${JSON.stringify(content.devServer.proxy)}'`
-                        );
-                    });
+                        if (content.devServer.proxy) {
+                            this.options.proxy = content.devServer.proxy
+                            await normalizeOptions.call(this)
+                            this.app._router.stack = this.app._router.stack.slice(0, this.baseRouteStackLength)
+                            setupMiddlewares.call(this, middlewares, devServer)
+
+                            console.log(
+                                `ProxyAutoPlugin: Successfully switch proxy to '${JSON.stringify(content.devServer.proxy)}'`,
+                            )
+                        }
+                    })
                 }
-            };
+            }
         }
-
     }
 }
 
-module.exports = ProxyAutoPlugin;
+module.exports = ProxyAutoPlugin
